e26ae3633ebdca9a4d73a1ca3d6a8eb77c587a34
Gayathri2005senthilkumar <gayathri.senthilkumar@openturf.in>
2025-07-24 09:20:32 +0530

    Fix Tailwind config file extension

--- src/api/ApiUrls.js
+++ src/api/ApiUrls.js
@@ -1,6 +1,7 @@
 const getAPIMap = (key) => {
   const map = {
     columnMapping: "/v1/report-config/column-mapping",
+    config: "/v1/report-config/report-config",
     // add more API paths here if needed
   };
   return map[key];
